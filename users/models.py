2111cea4d5b6df1bc46c9d0a710cc34c6a6405bc
Vikash Karodiya <45944283+vikash98k@users.noreply.github.com>
2021-12-08 23:09:27 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -5,6 +5,6 @@ class CustomUser(AbstractUser):
 	address = models.CharField(max_length=30)
 	email = models.EmailField(unique=True)
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = []
+	REQUIRED_FIELDS = ['username']
 	def __str__(self):
 		return self.username
